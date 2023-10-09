9d62bf6a7369d2601c0b570ba21324db5ee2d715
lzdmn2812 <145476264+lzdmn2812@users.noreply.github.com>
2023-10-09 18:10:30 +0300

    модуль числа

---
+++ zadanie_5/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу вычисления модуля числа
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<0)
+{
+    number= number * -1;
+    Console.WriteLine("модуль вашего числа: " + number);
+}
+else 
+{
+    Console.Write("модуль вашего числа: " + number);
+}
