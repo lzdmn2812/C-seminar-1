f3ca36e2b7348fbb059562561184437b65ddc9b8
lzdmn2812 <145476264+lzdmn2812@users.noreply.github.com>
2023-10-09 16:33:40 +0300

    2 задание + перенесла первое

---
+++ zadanie_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу, которая на вход принимает число и выдает его квадрат
+// например: 4 -> 16; -3 -> 9;
+
+Console.WriteLine("введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number*number; 
+Console.WriteLine($"Квадрат числа {number} равен {result}");
+
---
+++ zadanie_2/Program.cs
@@ -0,0 +1,10 @@
+﻿//На вход принимаем два числа, проверяем является ли второе число квадратом первого
+
+Console.WriteLine("введите первое число: ");
+int number1= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+ 
+if (number2==number1*number1)
+Console.WriteLine("yes");
+else Console.WriteLine("no");
