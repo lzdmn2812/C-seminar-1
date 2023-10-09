3b9f99e9905f917f480716520d13039aae19defa
lzdmn2812 <145476264+lzdmn2812@users.noreply.github.com>
2023-10-09 17:09:14 +0300

    задание с днями недели через массив

--- zadanie_3/Program.cs
+++ zadanie_3/Program.cs
@@ -1,36 +1,42 @@
 ﻿//Программа, которая выдает день недели по заданному номеру
 Console.Write("Введите число от 1 до 7: ");
 int number = int.Parse(Console.ReadLine());
-
-if (number==1)
+string []days = {"Понеделник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+if (number>=1&&number<=7)
 {
-    Console.Write("Понедельник ");
+    Console.Write(days[number-1]);
 }
-else if (number==2)
-{
-    Console.Write("Вторник ");
-}
-else if (number==3)
-{
-    Console.Write("Среда ");
-}
-else if (number==4)
-{
-    Console.Write("Четверг ");
-}
-else if (number==5)
-{
-    Console.Write("Пятница ");
-}
-else if (number==6)
-{
-    Console.Write("Суббота ");
-}
-else if (number==7)
-{
-    Console.Write("Воскресенье ");
-}
-else 
-{
-    Console.Write("Вы ввели число меньше 1 или больше 7");
-}
+else Console.Write("Дня недели не существует");
+// if (number==1)
+// {
+//     Console.Write("Понедельник ");
+// }
+// else if (number==2)
+// {
+//     Console.Write("Вторник ");
+// }
+// else if (number==3)
+// {
+//     Console.Write("Среда ");
+// }
+// else if (number==4)
+// {
+//     Console.Write("Четверг ");
+// }
+// else if (number==5)
+// {
+//     Console.Write("Пятница ");
+// }
+// else if (number==6)
+// {
+//     Console.Write("Суббота ");
+// }
+// else if (number==7)
+// {
+//     Console.Write("Воскресенье ");
+// }
+// else 
+// {
+//     Console.Write("Вы ввели число меньше 1 или больше 7");
+// }
+// //можно убрать везде else, но сделать в конце проверку "если число меньше 0 и больше 7"
