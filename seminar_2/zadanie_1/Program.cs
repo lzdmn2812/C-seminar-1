1fc874a781994743adf91152aed8aa97ce9acfb0
lzdmn2812 <145476264+lzdmn2812@users.noreply.github.com>
2023-10-09 22:03:51 +0300

    перенос папок в семинар 2

---
+++ seminar_2/zadanie_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+int ReadInt(string msg) // объявляет фунцию ридинт и строковый параметр мсг
+{
+
+    Console.WriteLine(msg); //  пишет в консоль  параметр мсг
+    string numb = Console.ReadLine(); // считывает с консоли строки
+    int number; // ввели переменную
+    number = int.Parse(numb); //  преобразовали строки в числа
+    return number; //  возврат из функции
+    }
+int number= ReadInt("Введите треххзначное число"); // вызываем функцию РидИнт и передаем в эту функцию функцию текст, результат складываем  
+if (number > 99 && number < 1000)
+{
+    int LastDigit = number % 10; //  вводим переменную "Последнее число"
+    Console.WriteLine("LastDigit=" + LastDigit);
+}
+else Console.WriteLine("LastDigit not three digit");
---
+++ seminar_2/zadanie_2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int number = new Random().Next(10,100);
+Console.WriteLine("Сгенерированное число " + number);
+int firstDigit=number/10;
+int lastDigit=number%10;
+if (firstDigit>lastDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(lastDigit);
+}
