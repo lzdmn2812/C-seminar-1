253bfb4b0b8583e227733e48a0168474e4a01d25
lzdmn2812 <145476264+lzdmn2812@users.noreply.github.com>
2023-10-12 21:58:19 +0300

    задачи по третьему семинару

---
+++ seminar-3/zadanie_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+//  показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int readQuarter (string message) // Ввод переменных.
+{ 
+    System.Console.WriteLine(message); // Получаем данные из строки.
+    string value = Console.ReadLine(); // Вызов консольного ввода, value - значение, которое вводим.
+    return int.Parse(value); // возрвращаем переменную в int.
+}
+int quarter = readQuarter("Введите номер четверти: ");
+
+if (quarter>0 && quarter<5)
+{
+    switch(quarter){
+        case 1: Console.WriteLine("X > 0, Y > 0"); break;
+        case 2: Console.WriteLine("X < 0, Y > 0"); break;
+        case 3: Console.WriteLine("X < 0, Y <0"); break;
+        default: Console.WriteLine("X > 0, Y < 0"); break;
+    }
+    
+}
+else Console.WriteLine("Такой четверти не существует");
---
+++ seminar-3/zadanie_3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+double enterCoordinate(string message){ // Ввод переменных.
+    System.Console.WriteLine(message); // Получаем данные из строки.
+    string value = Console.ReadLine(); // Вызов консольного ввода, value - значение, которое вводим.
+    return double.Parse(value); // возрвращаем переменную в int.
+}
+double resultDistance(double x, double y, double x2, double y2){
+    double xDistance= (x - x2);
+    double yDistance= (y - y2);
+    double distance = Math.Sqrt(xDistance*xDistance + yDistance*yDistance);
+    return distance;
+}
+
+double x = enterCoordinate("Введите координату X: "); // вызов функции enterCoordinate
+double y = enterCoordinate("Введите координату Y: ");
+double x2 = enterCoordinate("Введите координату X2: "); // вызов функции enterCoordinate
+double y2 = enterCoordinate("Введите координату Y2: ");
+
+Console.WriteLine("Дистанция между двумя точками " + resultDistance(x, y, x2, y2));
+
+// Задача 3: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// double enterCoordinate(string message){ // Ввод переменных.
+//     System.Console.WriteLine(message); // Получаем данные из строки.
+//     string value = Console.ReadLine(); // Вызов консольного ввода, value - значение, которое вводим.
+//     return double.Parse(value); // возрвращаем переменную в int.
+// }
+
+// double resolveDistance(double x, double y, double x2, double y2){ // 
+//     double xDistance = (x - x2);
+//     double yDistance = (y - y2);
+//     double distance = Math.Sqrt(xDistance*xDistance + yDistance*yDistance);
+//     return distance;
+// }
+
+// double x = enterCoordinate("Введите координату X 1 координаты: "); // вызов функции enterCoordinate
+// double y = enterCoordinate("Введите координату Y 1 координаты: ");
+// double x2 = enterCoordinate("Введите координату X 2 координаты: "); // вызов функции enterCoordinate
+// double y2 = enterCoordinate("Введите координату Y 2 координаты: ");
+
+// System.Console.WriteLine("Дистанция между двумя точками " + resolveDistance(x, y, x2, y2));
