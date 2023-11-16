452c9093172c0474d72db3bd7ce246e0dc8df8bd
lzdmn2812 <145476264+lzdmn2812@users.noreply.github.com>
2023-11-16 21:42:16 +0300

    семинар 9

---
+++ seminar_9/zadanie_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+// рекурсивно выведет все натуральные числа в промежутке 
+// от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    int result=Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintNaturalNumbers(int m, int n)
+{
+    if (m<n)
+    {
+        return;
+    }
+    PrintNaturalNumbers(m, n-1);
+    System.Console.WriteLine($"{n}\t");
+}
+
+int m=ReadInt("Введите первое число: ");
+int n=ReadInt("Введите второе число: ");
+System.Console.WriteLine(PrintNaturalNumbers(m, n));
---
+++ seminar_9/zadanie_5/Program.cs
@@ -0,0 +1,27 @@
+﻿// Проверка на простое число: 
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    int result=Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+bool SimpleNumb(int numb, int div=0)
+{
+    if (div==0)
+    {
+        div=numb-1;
+    }
+    if(div==1)
+    {
+        return true;
+    }
+
+    return numb%div!=0 && SimpleNumb(numb,div-1);
+}
+
+int m=ReadInt("Введите число: ");
+Console.WriteLine(SimpleNumb(m,m-1))
